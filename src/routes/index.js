3704923eacea726d8405fd7cc3733fc49dbf65fe
FoxHere <vitor.lima777@gmail.com>
2023-05-09 09:55:41 -0300

    Esse é um backup

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,8 +6,11 @@ const routes = (app) => {
   app.route("/").get((req, res) => {
     res.status(200).send({ titulo: "Curso de node" });
   });
-
+  
   app.use(express.json(), livros, autores);
+  app.use((req, res) => {
+    res.status(404).send("404 Página não encontrada");
+  })
 };
 
 export default routes;
